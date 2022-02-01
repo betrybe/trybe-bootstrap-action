8ddd0b2e0eb6edb6f906c48a1f45edd3afe5874f
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2022-02-01 17:17:29 -0300

    chore: ajusta action para suportar deploy de subdiretórios

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,33 +15,33 @@ fi
 # Section: Set Version
 if [[ "$ENVIRONMENT" == "preview-app" ]]; then
   VERSION=$(echo "${GITHUB_REF##*refs/heads/}" | tr '/_' '-' | tr [:upper:] [:lower:])
-  VALUES_FILE="chart/values-preview-apps.yaml"
-  
+  VALUES_FILE="$PREFIX_DIR/chart/values-preview-apps.yaml"
+
   # default hostname for preview-apps
-  PREVIEW_APP_HOSTNAME=$REPOSITORY-preview-app-$VERSION.betrybe.dev 
+  PREVIEW_APP_HOSTNAME=$REPOSITORY-preview-app-$VERSION.betrybe.dev
 
 elif [[ "$ENVIRONMENT" == "staging" ]]; then
   VERSION="staging"
-  VALUES_FILE="chart/values-staging.yaml"
-  CHART_FILE="chart/"
+  VALUES_FILE="$PREFIX_DIR/chart/values-staging.yaml"
+  CHART_FILE="$PREFIX_DIR/chart/"
 
 elif [[ "$ENVIRONMENT" == "homologation" ]]; then
   VERSION="homologation"
-  VALUES_FILE="chart/values-homologation.yaml"
-  CHART_FILE="chart/"
+  VALUES_FILE="$PREFIX_DIR/chart/values-homologation.yaml"
+  CHART_FILE="$PREFIX_DIR/chart/"
 
 else
   VERSION=${GITHUB_SHA:0:9}
-  VALUES_FILE="chart/values-production.yaml"
+  VALUES_FILE="$PREFIX_DIR/chart/values-production.yaml"
 
 fi
 
 # Helm Linter
-# helm lint chart/ --strict --values $VALUES_FILE FIX ME
+# helm lint $PREFIX_DIR/chart/ --strict --values $VALUES_FILE FIX ME
 
 # Generate a helm "package" for preview apps and production
 if [[ "$ENVIRONMENT" == "preview-app" || "$ENVIRONMENT" == "production" ]]; then
-  CHART_FILE=$(helm package chart/ --app-version=$VERSION | awk -F"/" '{print $NF}')
+  CHART_FILE=$(helm package $PREFIX_DIR/chart/ --app-version=$VERSION | awk -F"/" '{print $NF}')
 fi
 
 # Setting environment variables.
@@ -50,7 +50,7 @@ echo "VERSION=$VERSION" >> $GITHUB_ENV
 echo "CHART_FILE=$CHART_FILE" >> $GITHUB_ENV
 echo "VALUES_FILE=$VALUES_FILE" >> $GITHUB_ENV
 echo "PREVIEW_APP_HOSTNAME=$PREVIEW_APP_HOSTNAME" >> $GITHUB_ENV
-
+echo "SECRET_SENTRY_RELEASE=$VERSION" >> $GITHUB_ENV
 echo '
  _____            _
 /__   \_ __ _   _| |__   ___
