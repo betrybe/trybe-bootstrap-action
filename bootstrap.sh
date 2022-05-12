c3302fc16e23a1f6540e56c4b488940ddd2b55f2
diegohd87 <diego.dias@betrybe.com>
2022-05-12 12:36:35 -0300

    asdf

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,8 +31,8 @@ chart_file=""
 preview_app_hostname=""
 if [[ "$ENVIRONMENT" == "preview-app" ]]; then
   version=$(echo "${GITHUB_REF##*refs/heads/}" | tr '/_' '-' | tr [:upper:] [:lower:])
-  #aux=$(echo "${GITHUB_REF##*refs/heads/}" | tr '/_' '-' | awk -F "/" '{print $3}')
-  aux=$(echo "${GITHUB_REF##*refs/heads/}") | awk -F "/" '{print $3}'
+  aux=$(echo "${GITHUB_REF##*refs/heads/}" | awk -F "/" '{print $3}')
+  #aux=$(echo "${GITHUB_REF##*refs/heads/}") | awk -F "/" '{print $3}'
   values_file="$sub_dir/chart/values-preview-apps.yaml"
 
   # Default hostname for preview-apps
